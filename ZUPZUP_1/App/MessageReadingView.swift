6ec5e9ebf5fc4e403cf3c558d55f1266edc056f3
SeungWoo <swkang6348@naver.com>
2025-01-21 22:21:56 +0900

    Add MessageReadingView

---
+++ ZUPZUP_1/App/MessageReadingView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageReadingView.swift
+//  ZUPZUP_1
+//
+//  Created by 강승우 on 1/21/25.
+//
+
+import SwiftUI
+
+struct MessageReadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageReadingView()
+}
