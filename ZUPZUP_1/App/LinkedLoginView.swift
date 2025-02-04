e4862cac765003f940fbde8c3fd51ecf7e505f36
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-02-05 01:47:05 +0900

    feat: 로그인
    로그인

--- ZUPZUP_1/App/LinkedLoginView.swift
+++ ZUPZUP_1/App/LinkedLoginView.swift
@@ -1,18 +1,68 @@
 //
 //  LinkedLoginView.swift
-//  ZUPZUP_1
+//  ZUPZUP
 //
-//  Created by 강승우 on 1/19/25.
+//  Created by 강승우 on 1/3/25.
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LinkedLoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                AppleSigninButton()
+                    .padding(.top)
+                    .padding(.bottom)
+              
+                HStack{
+                    Text("계정이 없으신가요? ")
+                        .foregroundColor(Color.gray)
+                        .padding(.leading)
+                        .font(.system(size: 16))
+                    NavigationLink(destination: LoginView()){
+                        Text("회원가입하기")
+                            .foregroundColor(Color.black)
+                            .font(.system(size: 15.5))
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                
+            }
+        }
+        
     }
 }
+struct AppleSigninButton: View{
+    var body: some View{
+        SignInWithAppleButton(onRequest: {request in request.requestedScopes=[.fullName,.email] }, onCompletion: {
+            result in switch result{
+            case .success(let authResults):
+                (print("success"))
+                switch authResults.credential{
+                case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                    let UserIdentifier = appleIDCredential.user
+                    let fullName = appleIDCredential.fullName
+                    let email = appleIDCredential.email
+                    let IdentityToken=String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                    let AuthorizationCode=String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                    
+                   
+                default:
+                    print("Unsupported credential type")
+                    
+                }
+            case .failure(let error ): print(error)
+            }
+        })
 
+        .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+                .cornerRadius(5)
+                
+    }
+}
 #Preview {
     LinkedLoginView()
 }
